a31b3acaf1de9f5ea750e1a2e584711e2db00fc6
Irko Palenius <ispal84@gmail.com>
2017-11-16 23:22:44 +0200

    ci(Building docs to gh-pages):

---
+++ .travis-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# setup ssh-agent and provide the GitHub deploy key
+eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_dc4c0df7bb33_key -iv $encrypted_dc4c0df7bb33_iv -in github_deploy_key.enc -out github_deploy_key -d
+chmod 600 github_deploy_key
+ssh-add github_deploy_key
+
+# commit the assets in build/ to the gh-pages branch and push to GitHub using SSH
+./node_modules/.bin/gh-pages -d docs/ -b gh-pages -r git@github.com:${TRAVIS_REPO_SLUG}.git
